97f14b4e6124bd62461c02c1f7b2580a6f4b4100
maryna.yelakova <maryna.yelakova@gmail.com>
2016-11-16 16:01:44 +0200

    add bower components to index.js

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
-
-app.use(express.static(__dirname + '/frontend/app'));
-
-app.listen(process.env.PORT || 3000,function () {
+app.set('port', (process.env.PORT || 3000));
+app.use(express.static(__dirname + '/../frontend/app'));
+app.use('/bower_components',  express.static(__dirname + '/../bower_components'));
+app.listen(app.get('port'), function () {
     console.log('Example app listening on port 3000!');
 });
+
