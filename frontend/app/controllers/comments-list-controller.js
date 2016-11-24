1a56b1a866fc4b5f858f2152146e8b85535cf675
maryna.yelakova <maryna.yelakova@gmail.com>
2016-11-24 17:59:59 +0200

    remove console.log

--- frontend/app/controllers/comments-list-controller.js
+++ frontend/app/controllers/comments-list-controller.js
@@ -5,9 +5,8 @@
         $scope.username = /^[a-zA-Z0-9\s]{3,50}$/;
         $scope.useremail = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
         $scope.usercomment = /\w+/;
-        $scope.checkName = function(localscope) {
+        $scope.checkName = function() {
             if ($scope.commentForm.name.$invalid) {
-                console.log(localscope);
                 flashService.error('Only latin symbols and  numbers length between 3 and 50', false);
             } else {
                 flashService.clearFlashMessage();
@@ -27,7 +26,7 @@
                 flashService.clearFlashMessage();
             }
         };
-        
+
         $scope.getCommentsByPage = function(page){
             return commentsService.getComments(page);
         };
