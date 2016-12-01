bb6d1db2b081d8d894b2bec2e80258aeeb728655
maryna.yelakova <maryna.yelakova@gmail.com>
2016-12-01 23:36:58 +0200

    resize pictures

--- frontend/app/controllers/comments-list-controller.js
+++ frontend/app/controllers/comments-list-controller.js
@@ -1,7 +1,7 @@
 (function() {
     'use strict';     
-    angular.module('com').controller('com.commentsCtrl',['$scope','flashService','commentList','numberOfComments','comments-list-service', commentsCtrl]);
-    function commentsCtrl($scope, flashService,commentList,numberOfComments,commentsService){
+    angular.module('com').controller('com.commentsCtrl',['$scope','flashService','commentList','numberOfComments','comments-list-service','$q', commentsCtrl]);
+    function commentsCtrl($scope, flashService,commentList,numberOfComments,commentsService,$q){
         $scope.username = /^[a-zA-Z0-9\s]{3,50}$/;
         $scope.useremail = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
         $scope.usercomment = /\w+/;
@@ -69,17 +69,83 @@
                 $scope.comments = response.data;
             });
         };
+        $scope.dataURItoBlob = function(dataURI) {
+            var byteString;
+            if (dataURI.split(',')[0].indexOf('base64') >= 0)
+                byteString = atob(dataURI.split(',')[1]);
+            else
+                byteString = unescape(dataURI.split(',')[1]);
+            var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+            var ia = new Uint8Array(byteString.length);
+            for (var i = 0; i < byteString.length; i++) {
+                ia[i] = byteString.charCodeAt(i);
+            }
+
+            return new Blob([ia], {type:mimeString});
+        };
+
+        $scope.resizeImage = function (origImage,type) {
+            var maxHeight = 320;
+            var maxWidth = 240;
+            var height = origImage.height;
+            var width = origImage.width;
+
+            if (width > height) {
+                if (width > maxWidth) {
+                    height = Math.round(height *= maxWidth / width);
+                    width = maxWidth;
+                }
+            } else {
+                if (height > maxHeight) {
+                    width = Math.round(width *= maxHeight / height);
+                    height = maxHeight;
+                }
+            }
+
+            $scope.canvas.width = width;
+            $scope.canvas.height = height;
+            $scope.ctx = $scope.canvas.getContext('2d');
+            $scope.canvas.ctx = $scope.ctx;
+            $scope.ctx.drawImage(origImage, 0, 0, width, height);
+
+            return $scope.canvas.toDataURL(type, 1);
+        };
+
+        $scope.fileRead  = function(file) {
+            var deferred = $q.defer();
+            var reader = new FileReader();
+            reader.onload = function() {
+                deferred.resolve(reader.result);
+            };
+            reader.readAsDataURL(file);
+            return deferred.promise;
+        };
+        $scope.loadImage = function(url){
+            var deferred = $q.defer();
+            var img = new Image();
+            img.src = url;
+            img.onload = function () {
+                deferred.resolve(img);
+            };
+            return deferred.promise;
+        };
 
         $scope.addComment = function(){
             if(angular.isObject($scope.createComment.attachment)){
-                commentsService.saveCommentWithFile($scope.createComment).then(function () {
-                    $scope.getCommentsByPage().then(function(response){
-                        $scope.comments = response.data;
-                        $scope.updateCommentsCount();
-                        $scope.createComment={};
-                        $("#media").val('');
-                        $scope.commentForm.$setPristine();
-                    });
+                var patt = /^image\/.*/g;
+                $scope.fileRead($scope.createComment.attachment).then(function(url){
+                    $scope.loadImage(url).then(function(img){
+                        $scope.createComment.attachment = $scope.dataURItoBlob($scope.resizeImage(img,$scope.createComment.attachment.type));
+                        commentsService.saveCommentWithFile($scope.createComment).then(function () {
+                            $scope.getCommentsByPage().then(function(response){
+                                $scope.comments = response.data;
+                                $scope.updateCommentsCount();
+                                $scope.createComment={};
+                                $("#media").val('');
+                                $scope.commentForm.$setPristine();
+                            });
+                        });
+                    })
                 });
             }else{
                 commentsService.saveComment($scope.createComment).then(function () {
@@ -92,6 +158,7 @@
                 });
             }
         };
+
         $scope.updateCommentsCount = function () {
             commentsService.getCommentsNumber().then(function(response){
                 $scope.numberComments = response.data[0].count;
@@ -135,6 +202,21 @@
                 $scope.updateCommentsCount();
             });
         };
+        $scope.isTxt = function(fileName){
+            var patt = /.*\.txt$/g;
+            return patt.test(fileName);
+        };
+        $scope.isPicture = function(fileName){
+            var patt = /.*\.jpeg/g;
+            return patt.test(fileName);
+        };
+        $scope.config = {
+            width: 240,
+            height: 320,
+            quality: 1
+        };
+
+ 
         $scope.angular = angular;
         $scope.comments = commentList.data;
         $scope.createComment = {};
@@ -144,7 +226,6 @@
         $scope.reverseSort = false;
         $scope.sortparam = 'date';
         $scope.updateLastPageNum();
-
-
+        $scope.canvas = document.getElementById('picture');
     }
 })();
